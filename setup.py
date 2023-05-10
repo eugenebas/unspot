a283f777907795e02e01f5977b3c81fea2d777e8
Yevgeniy Basargin <19927545+eugenebas@users.noreply.github.com>
2023-05-10 09:14:31 +0300

    added setup script

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='unspot',
+    version='0.1.0',
+    description='Python package for work with the Unspot service',
+    url='https://github.com/eugenebas/unspot',
+    author='eugenebas',
+    author_email='eugenebas.github@gmail.com',
+    license='MIT',
+    packages=['unspot'],
+)
